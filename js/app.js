9514f58323bde10d7f596f8dbd095784070b5761
Sarah Westbrook <sjwestbrook@users.noreply.github.com>
2017-01-18 12:58:22 -0700

    Create app.js

---
+++ js/app.js
@@ -0,0 +1,38 @@
+var pomerleauApp = angular.module('pomerleauApp', ['ui.router']);
+
+pomerleauApp.config(function($stateProvider, $urlRouterProvider) {
+    
+    $urlRouterProvider.otherwise('/home');
+    
+    $stateProvider
+        
+        .state('home', {
+            url: '/home',
+            templateUrl: './partials/home.html'
+        })
+        
+        .state('civil-infrastructure', {
+            url: '/civil-infrastructure',
+            templateUrl: './partials/civil-infrastructure.html'
+        })
+        
+        .state('buildings', {
+            url: '/buildings',
+            templateUrl: './partials/buildings.html'
+        })
+        
+        .state('projects', {
+            url: '/projects',
+            templateUrl: './partials/projects.html'
+        })
+        
+        .state('competencies', {
+            url: '/competencies',
+            templateUrl: './partials/competencies.html'
+        })
+        
+        .state('about', {
+            url: '/about',
+            templateUrl: './partials/about.html'
+        });
+});
